737bbdb93c4d0b9284813f4baae69a2362acfeb5
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2023-10-15 12:33:49 +0200

    Advanced queries

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script>
 import { useQuery } from '@vue/apollo-composable'
 import ALL_BOOKS_QUERY from './graphql/allBooks.query.gql'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 // import ALL_AUTHORS_QUERY from './graphql/allAuthors.query.gql'
 
 export default {
   name: 'App',
   setup() {
-    const searchTerm = ref('') // create a reactive property with 'ref'
-    const { result, loading, error } = useQuery(ALL_BOOKS_QUERY, () => ({ search: searchTerm.value }))
+  const searchTerm = ref('')
+  const { result, loading, error } = useQuery(
+    ALL_BOOKS_QUERY,
+    () => ({
+      search: searchTerm.value,
+    }),
+    () => ({
+      debounce: 500,
+      enabled: searchTerm.value.length > 2,
+    })
+  )
+  const books = computed(() => result.value?.allBooks ?? [])
 
-    return { result, searchTerm, loading, error } // don't forget to return 'searchTerm'!
-  },
+  return { books, searchTerm, loading, error }
+},
 }
 
 </script>
@@ -22,7 +32,7 @@ export default {
     <p v-if="loading">Loading...</p>
     <p v-else-if="error">Something went wrong! Please try again</p>
     <template v-else>
-      <p v-for="book in result.allBooks" :key="book.id">
+      <p v-for="book in books" :key="book.id">
         {{ book.title }}
       </p>
     </template>
